692b40bf3b6aa63b9e5ea8bf8a9cf803218adc9d
xinke <wangxinyu2017@gmail.com>
2023-11-29 17:54:45 +1030

    bump to v0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='pjtools',
-      version='0.0.2',
+      version='0.0.3',
       author='Xinyu Wang',
       author_email='wangxinyu2017@gmail.com',
       packages=find_packages(),
